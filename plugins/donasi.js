fc2f1e2d0a4e43ce02e299074407c73625bb1c05
LeoPyth <LeoTyps@gmail.com>
2023-01-01 20:22:37 +0700

    FZ3

---
+++ plugins/donasi.js
@@ -0,0 +1,26 @@
+import fs from 'fs';
+let handler = async (m, { conn, text, usedPrefix }) => {
+
+    let img = fs.readFileSync('./thumbnail.jpg');
+    let img2 = 'https://telegra.ph/file/60c73df2ecbefd5e7d21a.jpg'
+    return conn.sendButton(m.chat, 'Scan QR Di atas\n❏ Donasi minimal 500p :v', donate, img2, [["MENU", usedPrefix + "menu"], ["OWNER", usedPrefix + "owner"]], m, {
+        contextInfo: {
+            externalAdReply: {
+                mediaUrl: "https://instagram.com/fangzganz",
+                mediaType: "VIDEO",
+                title: 'Donasi To Maxxy Botz ♥',
+                body: bottime,
+                previewType: 0,
+                thumbnail: img
+            }
+        }
+    })
+}
+handler.help = ['donasi']
+
+handler.tags = ['info']
+
+handler.command = /^(donasi|donate)$/i
+
+
+export default handler
